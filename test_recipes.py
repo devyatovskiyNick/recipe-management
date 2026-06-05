77e700ff74b4d183cb986a8afa03d54b7831b725
Azrael <dkolya2527@gmail.com>
2026-06-05 03:13:03 +0300

    реализовал тесты класса Recipe

--- test_recipes.py
+++ test_recipes.py
@@ -1,4 +1,7 @@
+import pytest
+
 from Ingredient import Ingredient
+from recipe import Recipe
 
 def test_create_ingredient():
     ingredient = Ingredient("Соль", 822, "мг")
@@ -7,12 +10,10 @@ def test_create_ingredient():
     assert ingredient.quantity == 822.0
     assert ingredient.unit == "мг"
 
-
 def test_str():
     ingredient = Ingredient("Соль", 822, "мг")
 
     assert str(ingredient) == "Соль: 822.0 мг"
-
 
 def test_eq():
     ingredient_1 = Ingredient("лук", 12, "г")
@@ -24,4 +25,51 @@ def test_eq():
     assert ingredient_1 == ingredient_2
     assert ingredient_1 != ingredient_3
     assert ingredient_1 != ingredient_4
+
+
+
+
+
+ingredients = [Ingredient("яйца", 2, "шт"), Ingredient("соль", 1, "ст.л"),
+                   Ingredient("помидоры", 0.5, "шт")]
+def test_create_recipe():
+    recipe = Recipe("яичница", ingredients)
+    assert recipe.title == "яичница"
+    assert recipe.ingredients == ingredients
+
+def test_len():
+    recipe = Recipe("яичница", ingredients)
+    assert len(recipe) == 3
+
+def test_add_ingredient():
+    recipe = Recipe("яичница", ingredients)
+
+    recipe.add_ingredient(Ingredient("лук", 12, "г"))
+    assert len(recipe) == 4
+    assert recipe.ingredients[3] == Ingredient("лук", 12, "г")
+
+    recipe.add_ingredient(Ingredient("яйца", 2, "шт"))
+    assert len(recipe) == 4
+    assert recipe.ingredients[0].quantity == 4
+
+def test_scale():
+    recipe = Recipe("яичница", ingredients)
+    scaled_recipe = recipe.scale(4.2)
+    assert recipe is not scaled_recipe
+    assert recipe.ingredients[0].quantity == 2
+
+
+    with pytest.raises(ValueError):
+        recipe.scale(0)
+    with pytest.raises(ValueError):
+        recipe.scale(-69)
+
+
+    scaled_recipe = recipe.scale(3)
+    assert len(recipe) == 3
+    assert scaled_recipe.ingredients[0].quantity == 6
+    assert scaled_recipe.ingredients[1].quantity == 3
+    assert scaled_recipe.ingredients[2].quantity == 1.5
+
+
 
